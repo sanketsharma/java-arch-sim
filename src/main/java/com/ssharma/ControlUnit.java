0a5f811cd3d420aaded0b6cf57e58d6d4272947c
Sanket Sharma <sanketsharma09@gmail.com>
2019-08-19 23:22:27 +0530

    Added the ControlUnit class

--- src/main/java/com/ssharma/ControlUnit.java
+++ src/main/java/com/ssharma/ControlUnit.java
@@ -1,4 +1,216 @@
 package com.ssharma;
 
+import com.ssharma.Exceptions.InvalidSignalException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class ControlUnit {
+    private static final Logger logger = LoggerFactory.getLogger(ControlUnit.class);
+    private final int  mask = 0xFC000000; //0b11111100000000000000000000000000
+    private final int rFormat = 0;
+    private final int lw = 0b1000110;
+    private final int sw = 0b101011;
+    private final int beq = 0b000100;
+
+    public ControlUnit() {
+        logger.info("ControlUnit inititalized");
+    }
+
+    public int getRegDst(int instruction){
+        int op = (instruction & mask) >> 26;
+        switch (op){
+            case rFormat:
+                return 1;
+
+            case lw:
+                return 0;
+
+            case sw:
+                return 0; //We don't care
+
+            case beq:
+                return 0; //We don't care
+
+            default: //Error
+                String message = "Invalid instruction "+ instruction + " encountered";
+                logger.error(message);
+                throw new InvalidSignalException(message);
+        }
+    }
+
+    public int getALUSrc(int instruction){
+        int op = (instruction & mask) >> 26;
+        switch (op){
+            case rFormat:
+                return 0;
+
+            case lw:
+                return 1;
+
+            case sw:
+                return 1;
+
+            case beq:
+                return 0;
+
+            default: //Error
+                String message = "Invalid instruction "+ instruction + " encountered";
+                logger.error(message);
+                throw new InvalidSignalException(message);
+        }
+    }
+
+    public int getMemtoReg(int instruction){
+        int op = (instruction & mask) >> 26;
+        switch (op){
+            case rFormat:
+                return 0;
+
+            case lw:
+                return 1;
+
+            case sw:
+                return 0; //We don't care
+
+            case beq:
+                return 0; //We don't care
+
+            default: //Error
+                String message = "Invalid instruction "+ instruction + " encountered";
+                logger.error(message);
+                throw new InvalidSignalException(message);
+        }
+    }
+
+    public int getRegWrite(int instruction){
+        int op = (instruction & mask) >> 26;
+        switch (op){
+            case rFormat:
+                return 1;
+
+            case lw:
+                return 1;
+
+            case sw:
+                return 0; //We don't care
+
+            case beq:
+                return 0; //We don't care
+
+            default: //Error
+                String message = "Invalid instruction "+ instruction + " encountered";
+                logger.error(message);
+                throw new InvalidSignalException(message);
+        }
+    }
+
+    public int getMemRead(int instruction){
+        int op = (instruction & mask) >> 26;
+        switch (op){
+            case rFormat:
+                return 0;
+
+            case lw:
+                return 1;
+
+            case sw:
+                return 0; //We don't care
+
+            case beq:
+                return 0; //We don't care
+
+            default: //Error
+                String message = "Invalid instruction "+ instruction + " encountered";
+                logger.error(message);
+                throw new InvalidSignalException(message);
+        }
+    }
+
+    public int getMemWrite(int instruction){
+        int op = (instruction & mask) >> 26;
+        switch (op){
+            case rFormat:
+                return 0;
+
+            case lw:
+                return 0;
+
+            case sw:
+                return 1; //We don't care
+
+            case beq:
+                return 0; //We don't care
+
+            default: //Error
+                String message = "Invalid instruction "+ instruction + " encountered";
+                logger.error(message);
+                throw new InvalidSignalException(message);
+        }
+    }
+
+    public int getBranch(int instruction){
+        int op = (instruction & mask) >> 26;
+        switch (op){
+            case rFormat:
+                return 0;
+
+            case lw:
+                return 0;
+
+            case sw:
+                return 0; //We don't care
+
+            case beq:
+                return 1; //We don't care
+
+            default: //Error
+                String message = "Invalid instruction "+ instruction + " encountered";
+                logger.error(message);
+                throw new InvalidSignalException(message);
+        }
+    }
+
+    public int getALUOp1(int instruction){
+        int op = (instruction & mask) >> 26;
+        switch (op){
+            case rFormat:
+                return 1;
+
+            case lw:
+                return 0;
+
+            case sw:
+                return 0; //We don't care
+
+            case beq:
+                return 0; //We don't care
+
+            default: //Error
+                String message = "Invalid instruction "+ instruction + " encountered";
+                logger.error(message);
+                throw new InvalidSignalException(message);
+        }
+    }
+
+    public int getALUOp0(int instruction){
+        int op = (instruction & mask) >> 26;
+        switch (op){
+            case rFormat:
+                return 0;
+
+            case lw:
+                return 0;
+
+            case sw:
+                return 0; //We don't care
+
+            case beq:
+                return 1; //We don't care
+
+            default: //Error
+                String message = "Invalid instruction "+ instruction + " encountered";
+                logger.error(message);
+                throw new InvalidSignalException(message);
+        }
+    }
 }
